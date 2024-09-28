a9b6eca5fcae5d03e47ce01e00246a8d61e81b4d
Lucas Liggi <lucasvechiliggi@gmail.com>
2024-09-28 15:18:20 -0300

    Adiciona tarefas restantes

--- aula06/ex2.php
+++ aula06/ex2.php
@@ -1,18 +1,21 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exercicio 2</title>
+    <title>Área de triângulo</title>
 </head>
 <body>
-<?php
-            function tring($n1,$n2) {
-                $res =$n1 * $n2/ 2;
-                return $res;
-            }
-        echo tring(5,10);
-        ?>
+    <?php
+        function area($x, $y) {
+            $z = ($x * $y)/2;
+            return $z;
+        }
+
+        echo "Área do triângulo de base 4 e altura 6 = " . area(4, 6) . "<br>";
+        echo "Área do triângulo de base 5 e altura 8 = " . area(5, 8) . "<br>";
+        echo "Área do triângulo de base 2 e altura 9 = " . area(2, 9) . "<br>";
+    ?>
 </body>
 </html>
--- aula06/ex4.php
+++ aula06/ex4.php
@@ -1,7 +1,23 @@
-<?php
-    $num=array(1,2,3,4)
-    function check($num){
-        $a=array_search(2,$num)
-    }
-    echo check($num)
-?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Checar numero em um array</title>
+</head>
+<body>
+    <?php 
+        function checagem($array, $numero) {
+            if (in_array($numero, $array)) {
+                return "True";
+            } else {
+                return "False";
+            }
+        }
+
+        echo checagem([1, 2, 3, 4, 5], 4) . "<br>";
+        echo checagem([1, 2, 3, 4, 5], 7) . "<br>";
+        echo checagem([1, 2, 3, 4, 5], 10) . "<br>";
+    ?>
+</body>
+</html>
